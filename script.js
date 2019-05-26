75296731a647ef21d037d32ff50813b6e734825c
V4Scale CI <ci@v4sacle.com>
2019-05-26 18:45:41 +0000

    Build from GitLab 63221731

--- script.js
+++ script.js
@@ -78,6 +78,41 @@ $(document).ready(function() {
 
     jsMenuNode.querySelector('.menu__toggle').addEventListener('click', callMenuToggle);
 })();
+jQuery(function($) {
+    function fix_size() {
+        var images = $('.fifthPagePicture img');
+        var images2 = $('.fifthPageLead img');
+        images.each(setsize);
+        images2.each(setsize);
+
+        function setsize() {
+            var img = $(this),
+                img_dom = img.get(0),
+                container = img.parents('.fifthPagePicture'),
+                container2 = img.parents('.fifthPageLead');
+            if (img_dom.complete) {
+                resize();
+            } else img.one('load', resize);
+
+            function resize() {
+                if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+                    img.width('100%');
+                    img.height('auto');
+                    return;
+                }
+                if ((container2.width() / container2.height()) < (img_dom.width / img_dom.height)) {
+                    img.width('100%');
+                    img.height('auto');
+                    return;
+                }
+                img.height('100%');
+                img.width('auto');
+            }
+        }
+    }
+    $(window).on('resize', fix_size);
+    fix_size();
+});
 
 // $(document).ready(function() {
 
