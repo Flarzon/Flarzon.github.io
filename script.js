002a347ce0dbe7c9247399c24f9e9edc5b355681
V4Scale CI <ci@v4sacle.com>
2019-06-12 08:47:24 +0000

    Build from GitLab 65821108

--- script.js
+++ script.js
@@ -218,3 +218,24 @@ $("body").on('click', '[href*="#"]', function(e) {
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top }, 1000);
     e.preventDefault();
 });
+
+
+function get_name_browser() {
+    // получаем данные userAgent
+    var ua = navigator.userAgent;
+    // с помощью регулярок проверяем наличие текста,
+    // соответствующие тому или иному браузеру
+    if (ua.search(/Chrome/) > 0) return 'Google Chrome';
+    if (ua.search(/Firefox/) > 0) return 'Firefox';
+    if (ua.search(/Opera/) > 0) return 'Opera';
+    if (ua.search(/Safari/) > 0) return 'Safari';
+    if (ua.search(/MSIE/) > 0) return 'Internet Explorer';
+    // условий может быть и больше.
+    // сейчас сделаны проверки только 
+    // для популярных браузеров
+    return 'Не определен';
+}
+
+// пример использования
+var browser = get_name_browser();
+if (browser == 'Safari' && $(window).height() > 1100) document.getElementById('thirdPageText').style.fontSize = '1rem';
